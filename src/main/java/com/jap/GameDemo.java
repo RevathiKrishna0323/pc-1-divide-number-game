bc1b95c9171c2705ce351507a218649b24ba44e6
Revathi G <revathikrishna0323@gmail.com>
2022-09-01 12:57:53 +0530

    properly formatted

--- src/main/java/com/jap/GameDemo.java
+++ src/main/java/com/jap/GameDemo.java
@@ -2,15 +2,15 @@ package com.jap;
 
 public class GameDemo {
 
-    public String divideByNumber(int firstNumber, int secondNumber){
+    public String divideByNumber(int firstNumber, int secondNumber) {
         //create local variable
         int finalNumber = 0;
         try {
             //write logic
             finalNumber = firstNumber / secondNumber;
-     //Handle specific exception
-        }catch (Exception exception){
-            if(firstNumber==0){
+            //Handle specific exception
+        } catch (Exception exception) {
+            if (firstNumber == 0) {
                 System.err.println("You can't divide zero with positive numbers");
             }
 
